4f8b425613b349587ccff12f6ff9fcb5d3a05ece
tuchys <70541515+tuchys@users.noreply.github.com>
2021-04-15 08:35:16 -0500

    Add files via upload

---
+++ Talleres/Interpolacion/polinomioLagrange2.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+from scipy.interpolate import lagrange
+import matplotlib.pyplot as plt
+
+x=np.array([4410000 , 4830000, 5250000, 5670000])
+y=np.array([1165978, 1329190, 1501474,1682830])
+
+polinomio=lagrange(x,y)
+xi=5000000
+yi=polinomio(xi)
+print(yi)
+
+xs=np.linspace(x.min(),x.max())
+ys=polinomio(xs)
+
+plt.plot(x,y,'o')
+plt.plot(xi,yi,'sr')
+plt.plot(xs,ys,':')
+
+plt.grid()
+plt.show()
+
+
