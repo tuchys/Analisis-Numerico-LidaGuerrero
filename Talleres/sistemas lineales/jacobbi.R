58831fac6d9c2843956a9f05cf3a4f5015a04a9e
tuchys <70541515+tuchys@users.noreply.github.com>
2021-08-04 12:01:00 -0500

    Delete Talleres directory

--- Talleres/sistemas lineales/jacobbi.R
+++
@@ -1,9 +0,0 @@
-# NOT RUN {
-install.packages('JacobiEigen')
-
-
-#V <- crossprod(matrix(c(4,-1,1, 3,7,3, 1,1,7), 3, 3))
-V <- crossprod(matrix(c(1,3,-2, 4,-1,1, 1,1,7 ), 3, 3))
-#Jacobi(V)
-eigjacobi(V)
-# }
